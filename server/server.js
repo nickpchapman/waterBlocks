3200192c574dd73b8bf3d9aff04aa414c158d5b2
Nick Chapman <nickpchapman@gmail.com>
2018-01-26 20:15:16 -0800

    Add bodyParser middleware

--- server/server.js
+++ server/server.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const bodyParser = require('body-parser');
 const helpers = require('./helpers.js');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('this is where the app goes');
-})
+});
 
 app.post('/getMap', (req, res) => {
-  console.log('called');
-  helpers.createMap('1, 2, 3, 4')
-
-
-})
+  console.log('called', req.body);
+  let map = helpers.createMap(req.body.walls);
+  res.send(map);
+});
 
 app.listen(port, console.log(`now live on port: ${port}!`));
