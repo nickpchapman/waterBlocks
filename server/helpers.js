33fa0f2b65c1b91170125e66f72729f208f8f0a3
Nick Chapman <nickpchapman@gmail.com>
2018-01-26 20:10:35 -0800

    Add stubs for server utility functions and create post route

---
+++ server/helpers.js
@@ -0,0 +1,26 @@
+const utils = require('./utilities.js');
+const helpers = {
+
+  createMap: walls => {
+
+  },
+
+  renderBlocks: (walls, map) => {
+
+  },
+
+  renderWater: (walls, map) => {
+
+  },
+
+  renderFlanks: (flanks, map) => {
+
+  },
+
+  addMapLabel: (map) => {
+
+  }
+
+};
+
+module.exports = helpers;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const helpers = require('./helpers.js');
+
 app.get('/', (req, res) => {
   res.send('this is where the app goes');
+})
+
+app.post('/getMap', (req, res) => {
+  console.log('called');
+  helpers.createMap('1, 2, 3, 4')
+
+
 })
 
 app.listen(port, console.log(`now live on port: ${port}!`));
