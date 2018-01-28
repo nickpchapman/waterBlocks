bb410f8aa430e20b953691e8a6b285265eb41e79
Nick Chapman <nickpchapman@gmail.com>
2018-01-27 17:01:20 -0800

    Add click handler for input box

---
+++ client/utilities.js
@@ -0,0 +1,21 @@
+document.getElementById('submit')
+  .addEventListener('click', () => {
+    handleClick();
+  });
+
+const handleClick = () => {
+    let walls = document.getElementById('walls').value
+    getNewMap(walls)
+};
+
+const getNewMap = () => {
+
+};
+
+const renderMap = () => {
+
+};
+
+const renderRow = () => {
+
+};
